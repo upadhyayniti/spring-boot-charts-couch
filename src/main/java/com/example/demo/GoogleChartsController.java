b471fd6147ab7758b066e83baa73d62dfbb45b3c
upadhyayniti <niti_u@hotmail.com>
2023-02-20 14:31:44 -0500

    querying db

--- src/main/java/com/example/demo/GoogleChartsController.java
+++ src/main/java/com/example/demo/GoogleChartsController.java
@@ -2,13 +2,50 @@ package com.example.demo;
 
 import java.util.Map;
 import java.util.TreeMap;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+
+import com.couchbase.client.java.query.QueryOptions;
+import com.couchbase.client.java.query.QueryScanConsistency;
+
+import com.example.demo.configs.DBProperties;
+import com.example.demo.models.CityPopulation;
 @Controller
 public class GoogleChartsController {
+
+    private Cluster cluster;
+    private Bucket bucket;
+    private DBProperties dbProperties;
+
+    public GoogleChartsController(Cluster cluster, Bucket bucket, DBProperties dbProperties) {
+        System.out.println("Initializing googlecharts controller, cluster: " + cluster + "; bucket: " + bucket);
+          this.cluster = cluster;
+          this.bucket = bucket;
+          this.dbProperties = dbProperties;
+      }
+
     @GetMapping("/")
     public String getPieChart(Model model) {
+
+        //select  distinct address.city, population from `cb-bucket`._default._default where population is not null
+        String qryString = "SELECT p.address.city, p.population* FROM `"+dbProperties.getBucketName()+"`.`_default`.`_default cp "+
+                            "WHERE population is not null";
+        System.out.println("Query="+qryString);
+        //TBD with params: final List<Profile> profiles = cluster.query("SELECT p.* FROM `$bucketName`.`_default`.`$collectionName` p WHERE lower(p.firstName) LIKE '$search' OR lower(p.lastName) LIKE '$search' LIMIT $limit OFFSET $skip",
+        final List<CityPopulation> cityPopulation = 
+                cluster.query(qryString,
+                    QueryOptions.queryOptions().scanConsistency(QueryScanConsistency.REQUEST_PLUS))
+                .rowsAs(CityPopulation.class);
+
         Map<String, Integer> graphData = new TreeMap<>();
         graphData.put("2016", 147);
         graphData.put("2017", 1256);
---
+++ src/main/java/com/example/demo/models/CityPopulation.java
@@ -0,0 +1,28 @@
+package com.example.demo.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.couchbase.core.mapping.Document;
+
+@Document
+public class CityPopulation {
+
+    @Id
+    private String city;
+    private String population;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+}
